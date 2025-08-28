14f3722eb8f8419c08f9a447fb290bf1dca49392
Takumi Takahashi <takahashi@ham.works>
2025-08-29 02:39:11 +0900

    ベースファイルの用意

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <h1>Cute Dog Images</h1>
+      </header>
+
+      <main>
+        <section>
+
+        </section>
+      </main>
+
+      <footer>
+        
+      </footer>
+    </div>
+  )
+}
+
+export default App;
